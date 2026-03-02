55f20fbb81194ad130a791d5572b2735820ade13
llucascr <lucasdecamposranzani@gmail.com>
2026-03-02 16:39:51 -0300

    feat: backend initialized and database connection implemented #1

---
+++ backend/src/test/java/com/planora/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planora.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
